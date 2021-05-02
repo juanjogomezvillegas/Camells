38475e8155441c5fb7d9e2e42b5467da71b392a6
juanjogomezvillegas <juanjogomvil@gmail.com>
2021-05-02 12:13:48 +0200

    main

--- HerenciaCamells.java
+++ HerenciaCamells.java
@@ -18,8 +18,10 @@ public class HerenciaCamells extends CarreraCamells {
     private int daltImg;
     private int daltLinies;
     private int baixLinies;
-    private int posicioEstrella;
     private int numero;
+    private int posicioEstrella;
+    private GImage camellGuanyador;
+    private int Guanyador;
 
     public static void main(String[] args) {
         new HerenciaCamells().start(args);
@@ -31,17 +33,29 @@ public class HerenciaCamells extends CarreraCamells {
 
     public void run() {
         ArrayList<String> rutaImatgesCamells = new ArrayList<>();
-
+        rutaImatgesCamells.add(ruta+"/Vermell.png");
         rutaImatgesCamells.add(ruta+"/Vermell.png");
         rutaImatgesCamells.add(ruta+"/Blau.png");
+        rutaImatgesCamells.add(ruta+"/Blau.png");
+        rutaImatgesCamells.add(ruta+"/Marro.png");
+        rutaImatgesCamells.add(ruta+"/Marro.png");
         rutaImatgesCamells.add(ruta+"/Marro.png");
         rutaImatgesCamells.add(ruta+"/Lila.png");
-        rutaImatgesCamells.add(ruta+"/Groc.png");
+
+        ArrayList<Integer> tipusCamells = new ArrayList<>();
+        tipusCamells.add(1);
+        tipusCamells.add(1);
+        tipusCamells.add(2);
+        tipusCamells.add(2);
+        tipusCamells.add(3);
+        tipusCamells.add(3);
+        tipusCamells.add(3);
+        tipusCamells.add(4);
 
         ArrayList<Camells> camells = new ArrayList<>();
 
-        for (String actual : rutaImatgesCamells) {
-            camells.add(new Camells(actual, getNumeroAleatori(1, 4)));
+        for (int i = 0, j = 0; i < rutaImatgesCamells.size() && j < tipusCamells.size(); i++, j++) {
+            camells.add(new Camells(rutaImatgesCamells.get(i), tipusCamells.get(j)));
         }
 
         ArrayList<GImage> ArrayCamellsImatge = new ArrayList<>();
@@ -51,5 +65,71 @@ public class HerenciaCamells extends CarreraCamells {
         }
 
         setPreparaPista(ArrayCamellsImatge);
+
+        setCarrera2(ArrayCamellsImatge, camells);
+    }
+
+    public void setCarrera2(ArrayList<GImage> ArrayCamellsImatge, ArrayList<Camells> camells) {
+        for (int x = 1; x <= 15; x++) {
+            for (int i = 0; i < ArrayCamellsImatge.size() && i < camells.size(); i++) {
+                if (camells.get(i).getTipusCamell() == 1) {
+                    numero = getNumeroAleatori(1, 50);
+                    ArrayCamellsImatge.get(i).move((numero * 2), 0);
+                    ArrayCamellsImatge.get(i).pause(5);
+                } else if (camells.get(i).getTipusCamell() == 2) {
+                    numero = getNumeroAleatori(5, 10);
+                    ArrayCamellsImatge.get(i).move(numero, 0);
+                    ArrayCamellsImatge.get(i).pause(5);
+                } else if (camells.get(i).getTipusCamell() == 3) {
+                    numero = getNumeroAleatori(1, 100);
+                    if ((numero % 2) == 0) {
+                        ArrayCamellsImatge.get(i).move(numero, 0);
+                    } else {
+                        ArrayCamellsImatge.get(i).move(2, 0);
+                    }
+                    ArrayCamellsImatge.get(i).pause(5);
+                } else if (camells.get(i).getTipusCamell() == 4) {
+                    ArrayCamellsImatge.get(i).move(1, 0);
+                    ArrayCamellsImatge.get(i).pause(5);
+                    ArrayCamellsImatge.get(i).move(-1, 0);
+                    ArrayCamellsImatge.get(i).pause(5);
+                }
+            }
+        }
+
+        for (int i = 1; i < ArrayCamellsImatge.size(); i++) {
+            if (i == 1) {
+                if (ArrayCamellsImatge.get(i).getX() > ArrayCamellsImatge.get(i-1).getX()) {
+                    camellGuanyador = ArrayCamellsImatge.get(i);
+                } else {
+                    camellGuanyador = ArrayCamellsImatge.get(i-1);
+                }
+            }
+            if (i > 1 && i < ArrayCamellsImatge.size()-1) {
+                if (camellGuanyador.getX() > ArrayCamellsImatge.get(i).getX()) {
+                    camellGuanyador = camellGuanyador;
+                } else {
+                    camellGuanyador = ArrayCamellsImatge.get(i);
+                }
+            }
+            if (i == ArrayCamellsImatge.size()-1) {
+                if (camellGuanyador.getX() > ArrayCamellsImatge.get(i).getX()) {
+                    posicioEstrella = (int) camellGuanyador.getY();
+                } else {
+                    posicioEstrella = (int) ArrayCamellsImatge.get(i).getY();
+                }
+            }
+        }
+
+        for (int i = 0; i < ArrayCamellsImatge.size(); i++) {
+            if (ArrayCamellsImatge.get(i).getY() == posicioEstrella) {
+                Guanyador = i;
+                break;
+            }
+        }
+
+        setMostraEstrella(posicioEstrella);
+
+        JOptionPane.showMessageDialog(null, "GUANYADOR: EL CAMELL "+(Guanyador+1));
     }
 }
