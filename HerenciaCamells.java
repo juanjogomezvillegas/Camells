240dcbf5099c6a3bbdfd5cfb982c97973806a48c
juanjogomezvillegas <juanjogomvil@gmail.com>
2021-05-03 11:14:04 +0200

    main

--- HerenciaCamells.java
+++ HerenciaCamells.java
@@ -75,7 +75,7 @@ public class HerenciaCamells extends CarreraCamells {
                 if (camells.get(i).getTipusCamell() == 1) {
                     numero = getNumeroAleatori(1, 50);
                     ArrayCamellsImatge.get(i).move((numero * 2), 0);
-                    ArrayCamellsImatge.get(i).pause(5);
+                    ArrayCamellsImatge.get(i).pause(10);
                 } else if (camells.get(i).getTipusCamell() == 2) {
                     numero = getNumeroAleatori(5, 10);
                     ArrayCamellsImatge.get(i).move(numero, 0);
@@ -90,9 +90,9 @@ public class HerenciaCamells extends CarreraCamells {
                     ArrayCamellsImatge.get(i).pause(5);
                 } else if (camells.get(i).getTipusCamell() == 4) {
                     ArrayCamellsImatge.get(i).move(1, 0);
-                    ArrayCamellsImatge.get(i).pause(5);
+                    ArrayCamellsImatge.get(i).pause(1);
                     ArrayCamellsImatge.get(i).move(-1, 0);
-                    ArrayCamellsImatge.get(i).pause(5);
+                    ArrayCamellsImatge.get(i).pause(1);
                 }
             }
         }
